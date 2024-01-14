7c5b0da77ccd6faa5b87567c0ab35ff0b05a762a
kartik <ky5986390@gmail.com>
2024-01-15 04:32:54 +0530

    Login Page -2:30

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Login from './Login';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Browse from './Browse';
 
 const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login/>,
+        },
+        {
+            path: "/browse",
+            element: <Browse/>,
+        },
+    ])
+    
   return (
-    <div className="text-center">Body</div>
+    <>
+    <RouterProvider router={appRouter}>
+
+    </RouterProvider>
+    </>
   )
 }
 
