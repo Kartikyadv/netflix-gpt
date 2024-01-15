5e0f84a8d45d24585746d5a7d453ad863f006349
Kartik Yadav <ky5986390@gmail.com>
2024-01-16 01:30:08 +0530

    signup logic firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCeok5Fze4uaBPn7mOv-efiROVFkKlkiQw",
   authDomain: "netflixgpt-906dc.firebaseapp.com",
@@ -17,5 +13,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth();
